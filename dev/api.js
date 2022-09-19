89784232b53f9be25c15dec4cd97f0648dcf2fdc
Deo <d.trinidad@lbteksystems.com>
2022-09-19 12:49:58 +0800

    Building The _GET -mine_ Endpoint -

--- dev/api.js
+++ dev/api.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
-
 const bitcoin = new Blockchain();
+const uuid = require('uuid/v1');
+
+const nodeAddress = uuid().split('-').join('');
 
 
 app.use(bodyParser.json());
@@ -30,7 +32,23 @@ app.post('/transaction', function(req, res){
 
 // mine a new block
 app.get('/mine', function(req, res){
+    const lastBlock = bitcoin.getLastBlock();
+    const previousBlockHash = lastBlock['hash'];
+    const currentBlockData = {
+        transactions: bitcoin.pendingTransactions,
+        index: lastBlock['index'] + 1
+    };
 
+    const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+    const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+
+    bitcoin.createNewTransaction(12.5, "00", nodeAddress);
+
+    const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
+    res.json({
+        note: "New block mined successfully",
+        block: newBlock
+    });
 });
 
 
