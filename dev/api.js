f7d9ecab9c0d2ebf79dd670e75b46267d079cbf8
Deo <d.trinidad@lbteksystems.com>
2022-09-19 09:35:21 +0800

    Building The _POST -transaction_ Endpoint

--- dev/api.js
+++ dev/api.js
@@ -23,8 +23,8 @@ app.get('/blockchain', function (req, res) {
 
 // create a new transaction
 app.post('/transaction', function(req, res){
-    console.log(req.body);
-    res.send(`The amount of the transaction is ${req.body.amount} bitcoin.`)
+   const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+   res.json({ note:`Transaction will be added in block ${blockIndex}.` });
 });
 
 
