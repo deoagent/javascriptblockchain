159a78dbae4361597b34079c5ad7f8c198a8f673
Deo <d.trinidad@lbteksystems.com>
2022-09-19 18:33:59 +0800

    Building _POST -register-nodes-bulk_ Endpoint

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -95,12 +95,19 @@ app.post('/register-node', function(req, res) {
     const nodeNotAlreadyPresent = bitcoin.networkNodes.indexOf(newNodeUrl) == -1;
     const notCurrentNode = bitcoin.currentNodeUrl !== newNodeUrl
     if (nodeNotAlreadyPresent && notCurrentNode) bitcoin.networkNodes.push(newNodeUrl);
-    res.json({ note: 'New node registered succezssfully.' });
+    res.json({ note: 'New node registered successsfully.' });
 });
 
 // register multiple nodes at once
 app.post('/register-nodes-bulk', function(req, res) {
+    const allNetworkNodes = req.body.allNetworkNodes;
+    allNetworkNodes.forEach(networkNodeUrl => {
+      const nodeNotAlreadyPresent = bitcoin.networkNodes.indexOf(networkNodeUrl) == -1;
+      const notCurrentNode = bitcoin.currentNodeUrl !== networkNodeUrl;
+      if(nodeNotAlreadyPresent && notCurrentNode) bitcoin.networkNodes.push(networkNodeUrl);
+    });
 
+    res.json({ note: 'Bulk registration successful.' });
 });
 
 app.listen(port, () => {
