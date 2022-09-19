744db300f3e4665d98911fb17de1ae8b76b818d7
Deo <d.trinidad@lbteksystems.com>
2022-09-19 09:20:45 +0800

    Building The _GET -blockchain_ Endpoint

--- dev/api.js
+++ dev/api.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//Welcome Page
+app.get('/', function (req, res) {
+  res.send('Welcome to Javascript blochain!');
+});
+
+
 //fetch entire blockchain
 app.get('/blockchain', function (req, res) {
-  
+  res.send(bitcoin);
 });
 
 
@@ -26,4 +36,5 @@ app.get('/mine', function(req, res){
 
 app.listen(3000, () => {
     console.log('Listening on port 3000.....');
+    console.log('http://localhost:3000');
 });
