a10b2fddbcdac405c31d3181d619776f764d58ac
Deo <d.trinidad@lbteksystems.com>
2022-09-19 18:07:23 +0800

    Building _POST -register-node_ Endpoint

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -91,7 +91,11 @@ app.post('/register-and-broadcast-node', function(req, res) {
 
 // register a node with the network
 app.post('/register-node', function(req, res) {
-
+    const newNodeUrl = req.body.newNodeUrl;
+    const nodeNotAlreadyPresent = bitcoin.networkNodes.indexOf(newNodeUrl) == -1;
+    const notCurrentNode = bitcoin.currentNodeUrl !== newNodeUrl
+    if (nodeNotAlreadyPresent && notCurrentNode) bitcoin.networkNodes.push(newNodeUrl);
+    res.json({ note: 'New node registered succezssfully.' });
 });
 
 // register multiple nodes at once
