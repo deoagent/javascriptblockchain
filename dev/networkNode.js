de82a305943128928b19054bdc6c6a44d6bf5c5f
Deo <d.trinidad@lbteksystems.com>
2022-09-20 13:53:12 +0800

    Building _POST -receive-new-block_ Endpoint

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -108,6 +108,28 @@ app.get('/mine', function(req, res){
 });
 
 
+//receive-new-block
+app.post('/receive-new-block', function(req, res) {
+    const newBlock = req.body.newBlock;
+    const lastBlock = bitcoin.getLastBlock();
+    const correctHash = lastBlock.hash === newBlock.previousBlockHash;
+    const correctIndex = lastBlock['index'] + 1 === newBlock['index'];
+
+    if (correctHash && correctIndex) {
+        bitcoin.chain.push(newBlock);
+        bitcoin.pendingTransactions = [];
+        res.json({
+            note: 'New block received and accepted.',
+            newBlock: newBlock
+        });
+    } else {
+        res.json({
+            note: 'New block rejected.',
+            newBlock: newBlock
+        });
+    }
+});
+
 //register a node and broadcast it in the whole network
 app.post('/register-and-broadcast-node', function(req, res) {
   const newNodeUrl = req.body.newNodeUrl;
