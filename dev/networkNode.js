1162cdc37508e9a3930b2bcb815b2b7028e12dc4
Deo <d.trinidad@lbteksystems.com>
2022-09-20 12:33:45 +0800

    Building _POST -transaction-broadcast_ Endpoint

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -32,10 +32,27 @@ app.post('/transaction', function(req, res){
 });
 
 //transaction broadcast
-// app.post('/transaction/broadcast', function(req, res) {
-//   const createNewTransaction = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+app.post('/transaction/broadcast', function(req, res) {
+  const newTransaction = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+  bitcoin.addTransactionToPendingTransactions(newTransaction);
 
-// });
+  const requestPromises = [];
+  bitcoin.networkNodes.forEach(networkNodeUrl => {
+    const requestOptions = {
+      uri: networkNodeUrl + '/transaction',
+      method: 'POST',
+      body: newTransaction,
+      json: true
+    };
+    
+    requestPromises.push(rp(requestOptions));
+  });
+
+  Promise.all(requestPromises)
+  .then(data => {
+      res.json({ note: 'Transaction created and broadcast successfully.' });
+  });
+});
 
 
 // mine a new block
