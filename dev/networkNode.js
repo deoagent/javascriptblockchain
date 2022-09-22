6d4fcda1e6bfef54f2953af6183a49e6842b3b99
Deo <d.trinidad@lbteksystems.com>
2022-09-22 11:28:56 +0800

    Adding The Block Explorer File

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -258,6 +258,10 @@ app.get('/address/:address', function(req, res) {
     });
 });
 
+app.get('/block-explorer', function(req, res) {
+    res.sendFile('./block-explorer/index.html', { root: __dirname })
+});
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}.....`);
 });
